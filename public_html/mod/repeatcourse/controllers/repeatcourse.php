86b938670962888a25a9cc997969de00a103b315
zhooravlik <zhooravlik@gmail.com>
2014-02-06 19:30:07 +0300

    made adding, removing repcourses into db, sliced, added some changed into html

--- public_html/mod/repeatcourse/controllers/repeatcourse.php
+++ public_html/mod/repeatcourse/controllers/repeatcourse.php
@@ -5,11 +5,16 @@ class repeatcourse_controller extends controller {
 
     function index() {
         global $DB ,$PAGE;
+        $curCoursesNames = "";
         $PAGE->requires->js("/lib/jquery/jquery-1.10.2.min.js");
         $PAGE->requires->js("/mod/repeatcourse/media/js/functions.js");
         $repeatCourseId = $DB->get_record('course_categories', array('name' => 'Repeat Courses'), 'id'); //courses from "Repeat Corse" category
-        $repeatCourses = $DB->get_records('course', array('category'=>$repeatCourseId->id), '', 'id, fullname');
         $curCourses = $DB->get_records_sql('SELECT id, name, ordering, cinterval FROM {repeatcourse} WHERE course = '.$repeatCourseId->id." ORDER BY ordering"); //courses from mod_repeatcourse table
+        foreach($curCourses as $c){
+            $curCoursesNames .= '"'.$c->name.'",';
+        }
+        $curCoursesNames = rtrim($curCoursesNames, ',');
+        $repeatCourses = $DB->get_records_sql('SELECT id, fullname FROM {course} WHERE category = "'.$repeatCourseId->id.'" AND fullname NOT IN('.$curCoursesNames.')');
 
         $this->get_view(array(
             'repeatCourses' => $repeatCourses,
@@ -34,7 +39,30 @@ class repeatcourse_controller extends controller {
         // To use the 'flash' message, provide a 3rd argument to redirect_to, containing the actual message:
         // $this->redirect_to( 'index', array('saved' => 1), array('notification' => get_string('message_saved', 'repeatcourse')));
     } // function create
+    
+    function add_repcourse(){
+        global $DB;
+        $repeatCourseId = $DB->get_record('course_categories', array('name' => 'Repeat Courses'), 'id');
+        $record = new stdClass();
+        $record->cinterval = optional_param('interval', 0, PARAM_INT);
+        $record->name = optional_param('coursename', '', PARAM_NOTAGS);
+        $record->introformat = 1;
+        $record->introformat = 0;
+        $record->timemodified = 0;
+        $record->ordering = 0;
+        $record->course = $repeatCourseId->id;
+        $insertedId = $DB->insert_record('repeatcourse', $record);
+        return $insertedId;
+    }
+    
+    function delete_repcourse(){
+        global $DB;
+        $repCourseId = optional_param('rep_id', 0, PARAM_INT);
+        if($repCourseId != 0){
+            return $DB->delete_records('repeatcourse', array('id' => $repCourseId));
+        } else{
+            return false;
+        }
+    }
 
 } // class repeatcourse_controller 
-
-?>
--- public_html/mod/repeatcourse/media/js/functions.js
+++ public_html/mod/repeatcourse/media/js/functions.js
@@ -1,9 +1,49 @@
 function add_rep_course() {
-    var table = $("#coursetable");
-    var row = $("#coursetable tr").eq(1).clone().appendTo(table);
-    row.children().eq(0).html("");
-    row.children().eq(1).html($("#course_selector :selected").html());
-    row.children().eq(3).html($("#repcourse_interval").val());
-	row.children().appendTo(table);
+    var id = 8; /* Change this value to value from $_GET!!!*/
+    var selected_course = $("#course_selector :selected").html();
+console.log(selected_course);
+    var data = "id="+id+"&coursename="+selected_course+"&interval="+$("#repcourse_interval").val()+"&action=add_repcourse";/*+"&ordering="+ordering;*/
+    $.ajax({
+        type: "POST",
+        url: '/mod/repeatcourse/index.php',
+        data: data,
+        success: function(e, data){
+            var table = $("#coursetable");
+            var row = $("#coursetable tr").eq(1).clone().appendTo(table);
+            row.children().eq(0).html();
+            row.children().eq(1).html(selected_course);
+            row.children().eq(3).html($("#repcourse_interval").val());
+            $("#course_selector :selected").remove();
+            if($("#course_selector").children().length == 0){
+                $("#available_courses").hide();
+            }
+            $("#result_msg").html('<span class="msg_success" style="display: none;">Successfully saved<span>');
+            $(".msg_success").fadeIn(500).fadeOut(2000).remove();
+        },
+        error: function(){
+            $("#result_msg").html('<span class="msg_error" style="display: none;">Something going wrong. Try again later.<span>');
+            $(".msg_error").fadeIn(500).fadeOut(2000).remove();
+        }
+    });
+    
+    return true;
+}
+
+function repcourse_remove(rep_id){
+    $.ajax({
+        type: "POST",
+        url: '/mod/repeatcourse/index.php',
+        data: "rep_id="+rep_id+"&action=delete_repcourse",
+        success: function(){
+            $("#repcourse_"+rep_id).remove();
+            $("#result_msg").html('<span class="msg_success" style="display: none;">Successfully removed<span>');
+            $(".msg_success").fadeIn(500).fadeOut(2000).remove();
+        },
+        error: function(){
+            $("#result_msg").html('<span class="msg_error" style="display: none;">Something going wrong. Try again later.<span>');
+            $(".msg_error").fadeIn(500).fadeOut(2000).remove();
+        }
+    });
+    
     return true;
 }
