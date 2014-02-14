c1feae2d9222dc4b587ff361fb0b0a398c511b1a
zhooravlik <zhooravlik@gmail.com>
2014-02-14 16:48:29 +0300

    Now this issue is almost completed. 1) "Main Course" is storing as instance of `course_module` to the table {repeatcourse}, all child courses are storing into {repeatcourse_records} 2) All courses from the category named "Repeat Course" are repeatcourses and their available for adding to each of "repeatcourse'-activity with necessary interval 3) Stored repeatcourses can be removed or reorderen on the adding page.

--- public_html/mod/repeatcourse/lib.php
+++ public_html/mod/repeatcourse/lib.php
@@ -5,21 +5,30 @@ require_once("class.repeatcourse.php");
 $repeatcourse_instance = new repeatcourse();
 
 function repeatcourse_cron(){
-    global $DB;
+    global $DB, $CFG;
+    require_once("/lib/moodlelib.php");
     $mailList = array();
     $curDateArr = getdate(time());
     
     $repCat = $DB->get_record('course_categories', array('name' => get_string('repcoursecategoryname', 'repeatcourse')), 'id');
-error_log(get_string('repcoursecategoryname', 'repeatcourse'));
-    $repCourses = $DB->get_records('courses', array('category' => $repCat->id), 'id, startdate');
+    $repCourses = $DB->get_records('courses', array('category' => $repCat->id), 'id, fullname, startdate');
     
     
     foreach($repCourses as $repcourse){
         $curInterval = $DB->get_record('repeatcourse_records', array('repeatcourse' => $repcourse->id), 'cinterval');
         $endDateArr = getdate($repcourse->startDate+($curInterval*86400));
         if($endDateArr['mday'] == $curDateArr['mday'] && $endDateArr['mon'] == $curDateArr['mon'] && $endDateArr['year'] == $curDateArr['year']){
-            $mailList[] = $DB->get_records('repeatcourse_records', array('repeatcourse' => $repcourse->id), '*');//TODO:Here is should be USERS, not COURSES:(
+            $curCourseUsers = $DB->get_records('course_completions', array('course' => $repcourse->id), 'userid');
+            foreach($curCourseUsers as $ccu){
+            	$mailTo = $DB->get_record('user', array('id' => $ccu->userid), 'email, firstname, lastname');
+            	
+            	$from = 'info@'.ltrim($_SERVER['SERVER_NAME'], 'www.');
+            	$subject = $mailTo->firstname . ', the course you\'ve passed is available again now!';
+            	$messagetext = 'Dear '.$mailTo->firstname . ' ' . $mailTo->lastname.',\n
+            		The course ' . $repcourse->fullname . ' you\'ve passed is available to be passed again now!\n
+					Best regards, HeartforLimburg team.';
+            	email_to_user($mail->email, $from, $subject, $messagetext);
+            }
         }
     }
-    
 }
