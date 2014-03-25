163ce66fd23306af91a2d0de6535a64aa70f9957
Onno Schuit <onno@solin.nl>
2014-03-25 19:45:29 +0100

    ignoring config file

--- public_html/config.php
+++ public_html/config.php
@@ -7,22 +7,22 @@ $CFG = new stdClass();
 $CFG->dbtype    = 'mysqli';
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = 'localhost';
-$CFG->dbname    = 'moodle';
+$CFG->dbname    = 'heart';
 $CFG->dbuser    = 'root';
-$CFG->dbpass    = '';
+$CFG->dbpass    = 'paarse';
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = array (
   'dbpersist' => 0,
   'dbsocket' => 0,
 );
 
-$CFG->wwwroot   = 'http://solin.loc';
-$CFG->dataroot  = 'D:\\OpenServer\\domains\\solin.loc\\topsdemo\\moodledata';
+$CFG->wwwroot   = 'http://heart';
+$CFG->dataroot  = '/home/onno/php/heart/moodledata';
 $CFG->admin     = 'admin';
 
 $CFG->directorypermissions = 0777;
 
-$CFG->passwordsaltmain = 'Bzbd0H(^_Gj3F2o>pM 3A7J>7Hd';
+//$CFG->passwordsaltmain = 'Bzbd0H(^_Gj3F2o>pM 3A7J>7Hd';
 
 require_once(dirname(__FILE__) . '/lib/setup.php');
 
